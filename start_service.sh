7ba1aa7c47347cf3ebcb0567abf6de4d0cd72303
chenmengchen <mengchen.chen@nexttao.com>
2019-09-02 13:10:09 +0800

    调整脚本

--- start_service.sh
+++ start_service.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python3 /code/service.py &
-envoy --v2-config-only -l info -c /etc/service-envoy.yaml --service-node serviceA-service --service-cluster serviceA-cluster
+python /code/service.py &
+envoy -l info -c /etc/service-envoy.yaml --service-node serviceA-service --service-cluster serviceA-cluster
